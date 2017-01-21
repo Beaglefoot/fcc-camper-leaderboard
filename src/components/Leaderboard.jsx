7b9dce0445713bfdedfe5960791df1db239daf7f
Beaglefoot <ch-e-st@yandex.ru>
2017-01-21 23:47:22 +0300

    feat: Show campers on leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 
 export default class Leaderboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { campers: [] };
+  }
+
+  componentDidMount() {
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(response => response.json())
+      .then(campers => this.setState({ campers }));
+  }
+
   render() {
+    const campersList = this.state.campers.map((camper, index) => (
+      <tr key={camper.username}>
+        <th scope="row">{++index}</th>
+        <td>
+          <img src={camper.img} className="img-circle" width="32px" height="32px" />
+          {' '}
+          {camper.username}
+        </td>
+        <td className="text-center">{camper.recent}</td>
+        <td className="text-center">{camper.alltime}</td>
+      </tr>
+    ));
+
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>#</th>
             <th>Camper</th>
-            <th>Points in the past 30 days</th>
-            <th>Total points</th>
+            <th className="text-center">Points in the past 30 days</th>
+            <th className="text-center">Total points</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>1</th>
-            <th>chmo</th>
-            <th>1</th>
-            <th>1</th>
-          </tr>
-          <tr>
-            <th>2</th>
-            <th>chmoshnik</th>
-            <th>2</th>
-            <th>2</th>
-          </tr>
+          {campersList}
         </tbody>
       </table>
     );
